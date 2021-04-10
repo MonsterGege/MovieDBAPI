c650037737cc5b9f3b629431e489287946312d82
Dyota Vidyadhari <672018378@student.uksw.edu>
2021-04-10 23:07:17 +0700

    added Bottom Navigation

--- app/src/main/java/com/example/tr3sister/MainActivity.java
+++ app/src/main/java/com/example/tr3sister/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tr3sister;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.page_top_rated:
+
+                        Toast.makeText(MainActivity.this, "Top Rated", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.page_popular:
+                        Toast.makeText(MainActivity.this, "Popular", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.page_upcoming:
+                        Toast.makeText(MainActivity.this, "Upcoming", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
